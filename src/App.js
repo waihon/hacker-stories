d4f3263ff3c7dbb2be40a48d015a4a6cdf38ff01
Waihon Yew <yewwaihon@gmail.com>
2022-07-16 17:48:50 +0800

    Define a function and execute it within the curly braces

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import * as React from 'react';
 
-// Variables defined outside of the function's body will be
-// defined once and not every time the function is called.
-const welcome = {
-  greeting: "Hey",
-  title: "React"
+function getTitle(title) {
+  return title;
 }
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h1>Hello {getTitle("React")}</h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
