4f2091b2c9c03702589f52f59687dfbf2157a004
Waihon Yew <yewwaihon@gmail.com>
2022-07-17 21:53:19 +0800

    Declare handler function to handle onChange event of text input

--- src/App.js
+++ src/App.js
@@ -64,10 +64,17 @@ const App = () => (
 );
 
 const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: element)
+    console.log(event.target.value)
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
 };
